184f91464b9229cf93b54212d8525e3c515ca92c
AndreLTenorio <aluiztenorio@gmail.com>
2026-05-24 12:49:56 -0300

    AAA - Arrange, Act Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,23 +1,22 @@
 import { test, expect } from '@playwright/test'
 
+/// AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  //Arrange
   await page.goto('http://localhost:5173/')
-
-  // Checkpoint  
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  // Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-
+    // Act
   await page.getByTestId('search-order-id').fill('VLO-CM7QV0')
-
   await page.getByTestId('search-order-button').click()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-CM7QV0')
 
+  // Assert
+  await expect(page.getByTestId('order-result-id')).toBeVisible()
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-CM7QV0')
+  
   await expect(page.getByTestId('order-result-status')).toBeVisible()
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 
